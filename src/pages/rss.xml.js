c682074694f6cb3176f307742ba5fb97daf7a9a9
Juan Daniel <mail.jdnielss@gmail.com>
2024-10-17 16:08:28 +0700

    chore: delete unused file

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,14 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
+	const portfolio = await getCollection('portfolio');
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
+		items: portfolio.map((portfolio) => ({
+			...portfolio.data,
+			link: `/portfolio/${portfolio.slug}/`,
 		})),
 	});
 }
