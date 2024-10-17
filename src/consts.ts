0c75e796d52fd62808e2672e43abb3228d278a0f
Juan Daniel <mail.jdnielss@gmail.com>
2024-10-17 23:33:45 +0700

    add: highlight feature

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Hivemind';
+export const PORTFOLIO_TITLE = 'Portfolio - Hivemind';
 export const SITE_DESCRIPTION = 'Hivemind Studio';
--- src/content/config.ts
+++ src/content/config.ts
@@ -10,6 +10,7 @@ const portfolio = defineCollection({
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
+		highlight: z.boolean(),
 	}),
 });
 
